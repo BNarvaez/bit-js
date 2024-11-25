773d8285713fb452001b37a26c4b24505dfdc1b5
Brayan Narvaez <131482664+BNarvaez@users.noreply.github.com>
2024-11-25 16:40:48 -0500

    javascript22

--- desafio.js
+++ desafio.js
@@ -39,10 +39,113 @@ function presentarse(nombre, edad){
 // Crear una función que reciba un nombre, un apellido y una edad por parámetros, la función debe imprimir en consola: Su nombre es ____ y usted tiene ____ años, si no se recibe la edad imprimir: Su nombre es ____ y usted tiene 100 años.
 
 function desafio4(nombre, apellido, edad = 100){
-  console.log(`Su nombre es ${nombre} ${apellido} y usted tiene ${edad} años.`)
+  //console.log(`Su nombre es ${nombre} ${apellido} y usted tiene ${edad} años.`)
 }
 
 desafio4('Juan', 'Perez', 25);
 desafio4('Juan', 'Perez')
 
 
+
+// Desafio #5
+
+//Crear una función que reciba dos párametros, un nombre y un apellido y retorne el nombre completo, primero el apellido y luego el nombre.
+
+
+
+
+
+
+
+
+
+// Desafio #6
+
+// Crear una función que reciba cuatro párametros, un nombre, una prenda, un color y una cantidad y retorne: ____ tiene __ ____ de color ____. Ej entrada: pepita, camisas, azul, 2 Ej salida: Pepita tiene 2 camisas de color azul.
+
+
+
+
+
+
+
+
+
+
+
+// Desafio #7
+
+// Crear una función para saludar, si la función recibe un nombre, imprimir por consola: Hola, ____! Si la función no recibe un nombre, imprimir: Hola, desconocido!
+
+
+
+
+
+
+
+
+
+
+
+
+// Desafio #8
+/* Crear 5 funciones, (sumar, restar, multiplicar, dividir, modulo), dede recibir 2 numeros e imprimir el resultado asi:
+Ej: entrada: 8,4
+Ej: salida: 8 + 4 = 12 */
+
+function sumar(a, b){
+    const resultado = a + b;
+    console.log(`${a} + ${b} = ${resultado}`);
+    
+}
+
+sumar(8, 4);
+sumar(100,25);
+
+function restar(a, b){
+    const resultado = a - b;
+    console.log(`${a} - ${b} = ${resultado}`);
+    
+}
+
+restar(8, 4);
+restar(100,25);
+
+function multiplicar(a, b){
+    const resultado = a * b;
+    console.log(`${a} x ${b} = ${resultado}`);
+    
+}
+
+multiplicar(8, 4);
+multiplicar(100,25);
+
+function dividir(a, b){
+    const resultado = a / b;
+    console.log(`${a} / ${b} = ${resultado}`);
+    
+}
+
+dividir(8, 4);
+dividir(100,25);
+
+function modular(a, b){
+    const resultado = a % b;
+    console.log(`${a} % ${b} = ${resultado}`);
+    
+}
+
+modular(8, 4);
+modular(100,25);
+
+function potenciar(a, b){
+    const resultado = a ** b;
+    console.log(`${a} ^${b} = ${resultado}`);
+    
+}
+
+potenciar(8, 4);
+potenciar(100,25);
+
+
+// PEMDAS
--- ejemplos.js
+++ ejemplos.js
@@ -1,7 +1,114 @@
+// Esto es un comentario de JavaScript de una sola línea
 
-function miFuncion1(nombre){
-    console.log(nombre);
+/*
+Esto es un comentario
+de JavaScript
+en varias líneas
+*/
+
+// en la siguiente línea estoy imprimiento un valor en consola
+//console.log('hola, a todos!');
+
+/* VARIABLES */
+// Declaración
+var nombre1;
+let nombre1a;
+
+// declaración e inicialización
+var nombre2 = 'pepita';
+let nombre2a = 'pepito';
+
+// variable de tipo constante
+const miConstante = 1;
+//console.log(miConstante);
+
+// automática
+// miVariableAutomatica;
+
+let miVariable = 'hola';
+//console.log(miVariable);
+miVariable = 'hello';
+//console.log(miVariable);
+
+/* TIPOS DE DATO PRIMITIVOS */
+miVariable = 'Hola, otra vez!';
+miVariable = "I'm";
+miVariable = 'Él dijo: "Hola"';
+miVariable = `Ella dijo: ${nombre2}`;
+miVariable = 'Él pensó en: ' + nombre2;
+miVariable = '1';
+miVariable = 'true';
+miVariable = '';
+
+miVariable = 0;
+miVariable = 10;
+miVariable = 9.5;
+miVariable = 0xff;
+miVariable = 123e5;
+miVariable = -10;
+
+miVariable = true;
+miVariable = false;
+
+miVariable = undefined;
+//console.log(miVariable);
+
+/* FUNCIONES */
+function miFuncion() {
+  console.log('Pepita Pérez');
 }
 
-miFuncion1('Brayan')
+//miFuncion();
+
+function miFuncion1(nombre) {
+  console.log(nombre);
+}
+
+//miFuncion1('Pepita Pérez');
+//miFuncion1('Pepito Pérez');
+
+function miFuncion2(apellido = 'Pérez') {
+  console.log(apellido);
+}
+
+//miFuncion2('Álvarez');
+//miFuncion2();
+
+function miFuncion3() {
+  return 'cadena de texto';
+}
+
+let valorRetornado = miFuncion3();
+//console.log(valorRetornado);
+
+/* OPERADORES */
+// aritméticos
+//console.log(8 + 4);
+//console.log(8 - 4);
+//console.log(8 * 4);
+//console.log(8 / 4);
+//console.log(8 % 4);
+//console.log(10 % 3);
+//console.log(2 ** 3);
+
+// PEMDAS
+
+// asignación
+miVariable = true;
+
+// concatenación
+//console.log('Hola, ' + '¿cómo estás?');
+//console.log('8' + '4');
+//console.log(8 + '4');
+//console.log('8' + 4);
+
+// llamado
+//console.log();
+
+
+
+
+
+
+
 
