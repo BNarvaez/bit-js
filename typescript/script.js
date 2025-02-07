10ccf73a3f584492eb95d4c5b5d4959bb2f04de2
Brayan Narvaez <131482664+BNarvaez@users.noreply.github.com>
2025-02-06 21:03:28 -0500

    Update agregamos el tema de typescript

--- typescript/script.js
+++ typescript/script.js
@@ -60,20 +60,105 @@ console.log(miTupla); */
 // objetos
 /* let miObject: object;
 miObject = {};
-console.log(miObject); */
-var objPersona1;
+console.log(miObject);
+let objPersona1: object;
 objPersona1 = {};
 objPersona1 = {
-    nombre: 'pepita',
-    usuario: 'pepa',
-    contrasenia: '123',
+  nombre: 'pepita',
+  usuario: 'pepa',
+  contrasenia: '123',
 };
 console.log(objPersona1);
-var objPersona2;
+let objPersona2: {
+  usuario: string;
+  contrasenia: string;
+  edad: number;
+  activo: boolean;
+  idGenero?: string;
+};
 objPersona2 = {
-    usuario: 'anne',
-    contrasenia: 'ABC',
-    edad: 30,
-    activo: true,
+  usuario: 'anne',
+  contrasenia: 'ABC',
+  edad: 30,
+  activo: true,
 };
 console.log(objPersona2);
+
+
+
+/* Enums
+Guardamos o agrupamos constantes, los enums su nombre la primera letra va en mayúscula. Se pueden modificar los valores.
+*/
+/* enum Perfiles {
+    Administrador,
+    Vendedor,
+    Cliente,
+}
+console.log(Perfiles);
+console.log(Perfiles.Vendedor); */
+// Alias
+/* Permiten compartir tipos entre diferentes objetos y variables */
+/* type NombreEmpleado = string;
+type EdadEmpleado = number;
+type ActivoEmpleado = boolean;
+
+type Empleado = {
+    nombre: NombreEmpleado;
+    edad: EdadEmpleado;
+    activo: ActivoEmpleado;
+}
+
+let miEmpleado: Empleado;
+
+miEmpleado = {
+    nombre: 'Brayan',
+    edad: 27,
+    activo: true,
+}
+
+console.log(miEmpleado); */
+// Interfaces
+/* Compartir tipos. Los tipos Alias son diferentes a los interfaces es porque los alias son tipos primitivos y los interfaces no.*/
+/* interface Vendedor {
+    id: number;
+    nombre: string;
+    activo: boolean;
+    perfil: string;
+}
+
+let miVendedor: Vendedor = {
+    id: 1,
+    nombre: 'Brayan',
+    activo: true,
+    perfil: 'Vendedor',
+}
+
+console.log(miVendedor); */
+// Funciones
+/*  */
+var valorRetornado; // Cualquier tipo de valor
+/* function miFuncion():void {
+   return 100;
+   console.log('works')
+} */
+/* function miFuncion():string {
+    return 'works'
+} */
+/* function miFuncion():number {
+    return 10;
+} */
+/* function miFuncion():boolean {
+    return false;
+} */
+/* function miFuncion(a: number, b: number):number{
+    return a + b;
+}
+ valorRetornado = miFuncion(10,5);
+ console.log(valorRetornado); */
+// Casteo
+/* El proceso donde sobreescribimos tipos */
+/* let miVariable: unknown = 'abc';
+
+console.log(miVariable);
+console.log((miVariable as string).length);
+console.log((<string>miVariable).length); */
--- typescript/script.ts
+++ typescript/script.ts
@@ -69,7 +69,7 @@ console.log(miTupla); */
 // objetos
 /* let miObject: object;
 miObject = {};
-console.log(miObject); */
+console.log(miObject);
 let objPersona1: object;
 objPersona1 = {};
 objPersona1 = {
@@ -92,3 +92,105 @@ objPersona2 = {
   activo: true,
 };
 console.log(objPersona2);
+
+
+
+/* Enums 
+Guardamos o agrupamos constantes, los enums su nombre la primera letra va en mayúscula. Se pueden modificar los valores.
+*/
+
+/* enum Perfiles {
+    Administrador,
+    Vendedor,
+    Cliente,
+}
+console.log(Perfiles);
+console.log(Perfiles.Vendedor); */
+
+
+// Alias
+/* Permiten compartir tipos entre diferentes objetos y variables */
+
+/* type NombreEmpleado = string;
+type EdadEmpleado = number;
+type ActivoEmpleado = boolean;
+
+type Empleado = {
+    nombre: NombreEmpleado;
+    edad: EdadEmpleado;
+    activo: ActivoEmpleado;
+}
+
+let miEmpleado: Empleado;
+
+miEmpleado = {
+    nombre: 'Brayan',
+    edad: 27,
+    activo: true,
+}
+
+console.log(miEmpleado); */
+
+
+// Interfaces
+/* Compartir tipos. Los tipos Alias son diferentes a los interfaces es porque los alias son tipos primitivos y los interfaces no.*/
+
+/* interface Vendedor {
+    id: number;
+    nombre: string;
+    activo: boolean;
+    perfil: string;
+}
+
+let miVendedor: Vendedor = {
+    id: 1,
+    nombre: 'Brayan',
+    activo: true,
+    perfil: 'Vendedor',
+}
+
+console.log(miVendedor); */
+
+ // Funciones
+ /*  */
+
+ let valorRetornado: any; // Cualquier tipo de valor
+
+ /* function miFuncion():void {
+    return 100;
+    console.log('works')
+ } */
+
+/* function miFuncion():string {
+    return 'works'
+} */
+
+/* function miFuncion():number {
+    return 10;
+} */
+
+/* function miFuncion():boolean {
+    return false;
+} */
+
+/* function miFuncion(a: number, b: number):number{
+    return a + b;
+}
+ valorRetornado = miFuncion(10,5);
+ console.log(valorRetornado); */
+
+
+
+
+ // Casteo
+ /* El proceso donde sobreescribimos tipos */
+
+/* let miVariable: unknown = 'abc';
+
+console.log(miVariable);
+console.log((miVariable as string).length);
+console.log((<string>miVariable).length); */
+
+
+
+ 
